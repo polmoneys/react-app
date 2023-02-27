c05961ed1d20c4a5778c759e3c4a657553335d54
Pol Moneys <pmones@wideagency.com>
2023-02-27 08:29:10 +0100

    chores

--- src/system/utils/theme.ts
+++ src/system/utils/theme.ts
@@ -7,6 +7,60 @@ export const classes = (...params: unknown[]): string =>
 
 export const black = (percent: number): string => `rgba(0,0,0,${percent / 100})`
 
+/**
+ * Utility to fill lines with gradients according to a value
+ * <li data-width="44"/>
+ * <li data-width="89"/>
+ * hint: quick horizonatal bar charts
+ */
+
+export function gradient(
+  selector = 'li',
+  gradient = '90deg, #bf71ff 3%, #1cbfff, #6afff3 100%',
+  chart?: boolean,
+): void {
+  const lines = document.querySelectorAll(selector)
+
+  for (const line of lines as any) {
+    const width = line.dataset.width
+    const inverse = (100 / Number(width)) * 100
+    line.style.backgroundImage = `linear-gradient(${gradient})`
+    line.style.backgroundSize = `${inverse}% 100%`
+    if (chart !== undefined) {
+      line.style.width = `${width as number}%`
+    }
+  }
+}
+
+export type Colors = Record<'start' | 'end', string>
+export type Percentage = `${string}%`
+export type Direction = 'x' | 'y'
+export interface Stop {
+  stop: Percentage
+  color: string
+}
+export interface Stops extends Array<Stop> {}
+
+export const repeatGradient = (
+  colors: Colors,
+  size: Percentage,
+  direction: Direction,
+): string => `repeating-linear-gradient(
+    ${direction === 'x' ? '90deg' : '0deg'},
+    ${colors?.start ?? 'currentColor'}, 
+    ${colors?.start ?? 'currentColor'} ${size},
+    ${colors?.end ?? 'transparent'} ${size},
+    ${colors?.end ?? 'transparent'} ${Number(size.replace('%', '')) * 2}%)`
+
+export const customGradient = (
+  stops: Stops,
+  direction: Direction,
+): string => `repeating-linear-gradient(
+   ${direction === 'x' ? '90deg' : '0deg'},
+   ${stops
+     .reduce((acc, current) => acc + `${current.color} ${current.stop},`, '')
+     .slice(0, -1)}`
+
 export const get1Fr = (): number | string => {
   const grid = document.querySelector('.grid')
   if (grid != null) {
